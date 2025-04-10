175da27ab4464238aabd0e7286a8a896d1497114
ArtyomHloba <artemgloba2@gmail.com>
2025-04-10 11:37:28 +0300

    Add validation to payment card

--- client/src/validation/paymentValidation.js
+++ client/src/validation/paymentValidation.js
@@ -6,7 +6,19 @@ const paymentValidationSchema = Yup.object({
     .required("Card number is required"),
   expiryDate: Yup.string()
     .matches(/^(0[1-9]|1[0-2])\/\d{2}$/, "Expiry date must be in MM/YY format")
-    .required("Expiry date is required"),
+    .required("Expiry date is required")
+    .test("expiryDate", "Card is expired", value => {
+      if (!value) return false;
+
+      const [month, year] = value.split("/").map(Number);
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth() + 1;
+      const currentYear = currentDate.getFullYear() % 100;
+
+      return (
+        year > currentYear || (year === currentYear && month >= currentMonth)
+      );
+    }),
   cvv: Yup.string()
     .matches(/^\d{3}$/, "CVV must be 3 digits")
     .required("CVV is required"),
