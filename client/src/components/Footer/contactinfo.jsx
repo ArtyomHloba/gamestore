9f54d1ce21c20622bf1ab9d35e8e034ca919233f
ArtyomHloba <artemgloba2@gmail.com>
2025-04-10 11:52:06 +0300

    Refactor footer component

---
+++ client/src/components/Footer/contactinfo.jsx
@@ -0,0 +1,21 @@
+import { FaTelegramPlane, FaGithub, FaEnvelope } from "react-icons/fa";
+
+const contactInfo = [
+  {
+    icon: <FaTelegramPlane />,
+    link: "https://t.me/gamestore_support",
+    label: "Telegram Support",
+  },
+  {
+    icon: <FaGithub />,
+    link: "https://github.com/ArtyomHloba/gamestore.git",
+    label: "GitHub",
+  },
+  {
+    icon: <FaEnvelope />,
+    link: "mailto:gamestore@gmail.com",
+    label: "Email: gamestore@gmail.com",
+  },
+];
+
+export default contactInfo;
--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,23 +1,5 @@
 import styles from "./Footer.module.css";
-import { FaTelegramPlane, FaGithub, FaEnvelope } from "react-icons/fa";
-
-const contactInfo = [
-  {
-    icon: <FaTelegramPlane />,
-    link: "https://t.me/gamestore_support",
-    label: "Telegram Support",
-  },
-  {
-    icon: <FaGithub />,
-    link: "https://github.com/ArtyomHloba/gamestore.git",
-    label: "GitHub",
-  },
-  {
-    icon: <FaEnvelope />,
-    link: "mailto:gamestore@gmail.com",
-    label: "Email: gamestore@gmail.com",
-  },
-];
+import contactInfo from "./contactInfo";
 
 function Footer() {
   return (
