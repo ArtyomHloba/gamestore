d1ac9b40071a14408e63cf74d995c200c57e9bed
ArtyomHloba <artemgloba2@gmail.com>
2025-04-10 11:46:00 +0300

    Refactor slider component

--- client/src/components/GameSlider/index.jsx
+++ client/src/components/GameSlider/index.jsx
@@ -2,6 +2,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./GameSlider.module.css";
+import slides from "./slides";
 
 function GameSlider() {
   const sliderSettings = {
@@ -13,25 +14,6 @@ function GameSlider() {
     autoplaySpeed: 10000,
     pauseOnHover: true,
   };
-
-  const slides = [
-    {
-      title: "Doom The Dark Ages",
-      image: "https://images5.alphacoders.com/138/1389830.jpg",
-      description: "Coming Soon!",
-    },
-    {
-      title: "The Last Of Us 2",
-      image:
-        "https://i0.wp.com/caniplaythat.com/wp-content/uploads/2020/06/the-last-of-us-2-key-art-ellie-logo.jpg?fit=1920%2C1080&ssl=1",
-      description: "Action, Adventure",
-    },
-    {
-      title: "Spider-man 2",
-      image: "https://images7.alphacoders.com/131/1317406.jpeg",
-      description: "Action, Adventure",
-    },
-  ];
 
   return (
     <div className={styles.sliderContainer}>
---
+++ client/src/components/GameSlider/slides.js
@@ -0,0 +1,20 @@
+const slides = [
+  {
+    title: "Doom The Dark Ages",
+    image: "https://images5.alphacoders.com/138/1389830.jpg",
+    description: "Coming Soon!",
+  },
+  {
+    title: "The Last Of Us 2",
+    image:
+      "https://i0.wp.com/caniplaythat.com/wp-content/uploads/2020/06/the-last-of-us-2-key-art-ellie-logo.jpg?fit=1920%2C1080&ssl=1",
+    description: "Action, Adventure",
+  },
+  {
+    title: "Spider-man 2",
+    image: "https://images7.alphacoders.com/131/1317406.jpeg",
+    description: "Action, Adventure",
+  },
+];
+
+export default slides;
