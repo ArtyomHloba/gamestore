c57661f84530fbefd8df83672f78505d8bc05a81
ArtyomHloba <artemgloba2@gmail.com>
2025-04-11 15:11:13 +0300

    Add animations for logo + heart logo

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { supabase } from "../../supabaseClient";
 import { FaHeart } from "react-icons/fa";
+import { motion } from "framer-motion";
 import styles from "./Header.module.css";
 
 function Header() {
@@ -23,14 +24,29 @@ function Header() {
     <header className={styles.headerPage}>
       <div className={styles.logoContainer}>
         <Link to="/" className={styles.logo}>
-          <IoGameControllerOutline />
+          <motion.div
+            initial={{ rotate: 0, opacity: 0 }}
+            animate={{ rotate: 360, opacity: 1 }}
+            transition={{
+              duration: 1.5,
+              ease: "easeInOut",
+            }}
+          >
+            <IoGameControllerOutline size={40} />
+          </motion.div>
         </Link>
         <p className={styles.gameStore}>Game Store</p>
       </div>
 
       <div className={styles.signUpContainer}>
         <Link to="/wishlist">
-          <FaHeart color="red" className={styles.wishlistButton} />
+          <motion.div
+            whileHover={{ scale: 1.2, rotate: 10 }}
+            whileTap={{ scale: 0.9, rotate: -10 }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            <FaHeart color="red" className={styles.wishlistButton} />
+          </motion.div>
         </Link>
         <Link to="/my-games">
           <button className={styles.loginBtn}>Profile</button>
